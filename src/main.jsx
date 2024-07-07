c6331e668290b5e67dc410aae7af232aa2cf50c8
Klee Collage <klee.collage@gmail.com>
2024-07-07 16:58:49 -0500

    staged
    Unnecessary files are removed and bootstrap is included in the index.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import { HooksApp } from './HooksApp.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<HooksApp />
+	</React.StrictMode>
+);
