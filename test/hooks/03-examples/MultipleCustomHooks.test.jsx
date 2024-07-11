593f257292b19b412f9c6a587649cbab88569084
Klee Collage <klee.collage@gmail.com>
2024-07-11 01:19:17 -0500

    Testing staged
    Added dependencies for testing and performed tests on the MultipleCustomHooks component and customHooks (useCounter and useForm)

---
+++ test/hooks/03-examples/MultipleCustomHooks.test.jsx
@@ -0,0 +1,82 @@
+import { fireEvent, screen } from "@testing-library/dom"
+import { render } from "@testing-library/react"
+import { MultipleCustomHooks } from "../../../src/03-examples/MultipleCustomHooks"
+import { useFetch } from "../../../src/hooks/useFetch"
+import { useCounter } from "../../../src/hooks/useCounter"
+
+jest.mock('../../../src/hooks/useFetch.js')
+jest.mock('../../../src/hooks/useCounter.js')
+
+describe('Prueba sobre el componente <MultipleCustomHooks/>', () => { 
+    
+    const mockIncrement = jest.fn()
+    
+    useCounter.mockReturnValue({
+        counter: 1,
+        increment: mockIncrement
+    })
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+    
+    // **************************************************** //
+    test('debe mostrar el componente por defecto', () => {
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: false,
+            error: null,
+        })
+        render(<MultipleCustomHooks />)
+        // screen.debug()
+        // evaluamos el estado inicial en lugar de hacer snapshot
+        expect(screen.getByText('Informacion de pokemons'));
+        expect(screen.getByText('Cargando'))
+        expect(screen.getAllByRole('button', { name:'Anterior' }))
+        expect(screen.getAllByRole('button', { name:'Siguiente' }))
+    })
+    // **************************************************** //
+    test('debe mostrar un pokemon', () => { 
+        useFetch.mockReturnValue({
+            data: {
+                name: 'pikachu',
+                sprites:
+                    {
+                        front_default: "https://google.com",
+                        back_default: "https://imagenes.com",
+                        front_shiny: "https://googleimages.com",
+                        back_shiny: "https://pictionary.com",
+                    }
+            },
+            isLoading: false,
+        })
+        render(<MultipleCustomHooks />)
+        // screen.debug()
+        expect(screen.queryByText((content) => content.includes('pikachu'))).toBeTruthy();
+        expect((screen.getAllByRole('img'))).toHaveLength(4)
+     })
+    // **************************************************** //
+    test('debe de llamar la funcion de incrementar', () => {  
+        useFetch.mockReturnValue({
+            data: {
+                name: 'pikachu',
+                sprites:
+                    {
+                        front_default: "https://google.com",
+                        back_default: "https://imagenes.com",
+                        front_shiny: "https://googleimages.com",
+                        back_shiny: "https://pictionary.com",
+                    }
+            },
+            isLoading: false,
+        })
+
+        render(<MultipleCustomHooks />)
+        // screen.debug()
+        const nextBtn = screen.getByRole('button', { name: 'Siguiente' })
+        fireEvent.click(nextBtn);
+
+        expect(mockIncrement).toHaveBeenCalled()
+    })
+})
---
+++ test/hooks/useCounter.test.js
@@ -0,0 +1,58 @@
+import { act, renderHook } from "@testing-library/react"
+import { useCounter } from "../../src/hooks/useCounter"
+
+describe('Pruebas en custom Hook useCounter', () => { 
+    // ***************************************************** //
+    test('debe retornar los valores por defecto', () => { 
+        const { result } = renderHook(() => useCounter())
+        const { counter, decrement, increment, reset } = result.current
+        console.log(result)
+
+        expect(counter).toBe(10);
+        expect(decrement).toEqual(expect.any(Function));
+        expect(increment).toEqual(expect.any(Function));
+        expect(reset).toEqual(expect.any(Function));
+    })
+    // ***************************************************** //
+    test('debe generar al counter con el valor de 100', () => { 
+        const { result } = renderHook(() => useCounter(100))        
+        expect(result.current.counter).toBe(100);
+     })
+    // ***************************************************** //
+    test('debe de incrementar el contador', () => { 
+        const { result } = renderHook(() => useCounter(50))
+        const { increment, counter } = result.current
+
+        act(() => {
+            increment();
+            increment(3);
+        })
+        expect(result.current.counter).toBe(54)
+
+      })
+    // ***************************************************** //
+    test('debe de decrementar el contador', () => { 
+        const { result } = renderHook(() => useCounter(50))
+        const { decrement, counter } = result.current
+
+        act(() => {
+            decrement();
+            decrement(3);
+        })
+        expect(result.current.counter).toBe(46)
+      })
+    // ***************************************************** //
+    test('debe volver el contador al estado inicial', () => { 
+        const { result } = renderHook(() => useCounter(20))
+        const { reset, decrement, increment, counter } = result.current
+
+        act(() => { 
+            decrement(25);
+            increment(450);
+            reset()
+        })
+
+        expect(counter).toBe(20)
+
+     })
+})
